b57d302fc339cfd1d48eb78564c5615940a6dcab
Littlesun0160 <anastasya.speranskaya@yandex.ru>
2024-03-29 19:32:49 +0200

    add task 1 (point b)

--- LAB3/code/index.php
+++ LAB3/code/index.php
@@ -1,13 +1,22 @@
 <?php
-
-//Задание №1(1 пункт)
+echo '<h2>Задание №1(а)</h2>';
 $text = 'ahb acb aeb aeeb adcb axeb';
 $regexp = '/a[a-zA-Z]{2}b/';
 $matches = [];
 $count = preg_match_all($regexp, $text, $matches);
 echo "<b>Найдено строк:</b> {$count}<br/>";
-echo "<b>Найденные строки:</b> ";
+echo '<b>Найденные строки:</b> ';
 foreach ($matches[0] as &$str)
     echo "$str ";
 echo "<br/>";
+
+echo '<h2>Задание №1(b)</h2>';
+$text = 'a1b2c3';
+$regexp = '/[0-9]+/';
+//Чтобы возвести числа в куб, необходимо ввести функцию в месте $replacement
+$result = preg_replace_callback($regexp,
+    function ($matches)
+    {return pow($matches[0], 3);},
+    $text);
+echo "<b>Результат замены:</b> {$result}<br/>";
 ?>
