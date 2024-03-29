4c395abd0e020cb1eb3ef1132731b81e4b7af5b0
Littlesun0160 <anastasya.speranskaya@yandex.ru>
2024-03-29 18:42:12 +0200

    add task 1 (point a)

--- LAB3/code/index.php
+++ LAB3/code/index.php
@@ -1,2 +1,13 @@
 <?php
-echo phpinfo();
+
+//Задание №1(1 пункт)
+$text = 'ahb acb aeb aeeb adcb axeb';
+$regexp = '/a[a-zA-Z]{2}b/';
+$matches = [];
+$count = preg_match_all($regexp, $text, $matches);
+echo "<b>Найдено строк:</b> {$count}<br/>";
+echo "<b>Найденные строки:</b> ";
+foreach ($matches[0] as &$str)
+    echo "$str ";
+echo "<br/>";
+?>
